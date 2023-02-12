a58f32c77cb9b0e6b3f4ef0a3b2e85cc83620b4b
AuJung <natthaphong_th@cmu.ac.th>
2023-02-12 15:07:12 +0700

    The file has been prepared.

---
+++ src/Tokenizer/IterateTokenizer.java
@@ -0,0 +1,88 @@
+package Tokenizer;
+
+public class IterateTokenizer implements Tokenizer{
+    private String src, next;
+    private int pos;
+
+    public IterateTokenizer(String src) {
+        this.src = src;
+        pos = 0;
+        computeNext();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    @Override
+    public String peek(){
+        if(next == null)
+            throw new TokenizerException.NoToken();
+        return next;
+    }
+
+    @Override
+    public boolean peek(String s){
+        if (!hasNext()) {
+            return false;
+        } else {
+            return next.equals(s);
+        }
+    }
+
+    @Override
+    public String consume() {
+        if (!hasNext()) {
+            throw new TokenizerException.NoToken();
+        } else {
+            String result = next;
+            computeNext();
+            return result;
+        }
+    }
+
+    @Override
+    public boolean consume(String s){
+        if (!hasNext()) {
+            throw new TokenizerException.NoToken();
+        } else {
+            if (next.equals(s)) {
+                computeNext();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private void computeNext() {
+        if (src == null) return;
+        StringBuilder sb = new StringBuilder();
+        while (pos < src.length() && Character.isWhitespace(src.charAt(pos))) {
+            pos++;
+        }
+        if (pos == src.length()) {
+            next = null;
+            return;
+        }
+        char c = src.charAt(pos);
+        if (Character.isDigit(c)) {
+            while (pos < src.length() && Character.isDigit(src.charAt(pos))) {
+                sb.append(src.charAt(pos));
+                pos++;
+            }
+        } else if (Character.isLetter(c)) {
+            while (pos < src.length() && Character.isLetter(src.charAt(pos))) {
+                sb.append(src.charAt(pos));
+                pos++;
+            }
+        } else if ("()+-*/%^{}=".contains(String.valueOf(c))) {
+            sb.append(src.charAt(pos));
+            pos++;
+        } else {
+            throw new TokenizerException.BadCharacter(c);
+        }
+        next = sb.toString();
+    }
+}
---
+++ src/Tokenizer/IterateTokenizerTest.java
@@ -0,0 +1,72 @@
+package Tokenizer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IterateTokenizerTest {
+    private IterateTokenizer tkz;
+
+    @Test
+    public void testHasNext() {
+        tkz = new IterateTokenizer(null);
+        assertFalse(tkz.hasNext());
+        tkz = new IterateTokenizer("");
+        assertFalse(tkz.hasNext());
+        tkz = new IterateTokenizer("a");
+        assertTrue(tkz.hasNext());
+    }
+
+    @Test
+    public void testPeek() {
+        tkz = new IterateTokenizer(null);
+        assertThrows(TokenizerException.NoToken.class, tkz::peek);
+        tkz = new IterateTokenizer("");
+        assertThrows(TokenizerException.NoToken.class, tkz::peek);
+        tkz = new IterateTokenizer("a");
+        assertEquals("a", tkz.peek());
+    }
+
+    @Test
+    public void testPeekString() {
+        tkz = new IterateTokenizer(null);
+        assertFalse(tkz.peek(""));
+        assertFalse(tkz.peek("a"));
+        tkz = new IterateTokenizer("");
+        assertFalse(tkz.peek(""));
+        assertFalse(tkz.peek("a"));
+        tkz = new IterateTokenizer("a");
+        assertTrue(tkz.peek("a"));
+        tkz = new IterateTokenizer("abc");
+        assertFalse(tkz.peek("a"));
+        assertTrue(tkz.peek("abc"));
+    }
+
+    @Test
+    public void testConsume() {
+        tkz = new IterateTokenizer(null);
+        assertThrows(TokenizerException.NoToken.class, tkz::consume);
+        tkz = new IterateTokenizer("");
+        assertThrows(TokenizerException.NoToken.class, tkz::consume);
+        tkz = new IterateTokenizer("a");
+        assertEquals("a", tkz.consume());
+    }
+
+    @Test
+    public void testConsumeString() {
+        tkz = new IterateTokenizer(null);
+        assertThrows(TokenizerException.NoToken.class, () -> tkz.consume(""));
+        assertThrows(TokenizerException.NoToken.class, () -> tkz.consume("a"));
+        tkz = new IterateTokenizer("");
+        assertThrows(TokenizerException.NoToken.class, () -> tkz.consume(""));
+        assertThrows(TokenizerException.NoToken.class, () -> tkz.consume("a"));
+        tkz = new IterateTokenizer("a");
+        assertTrue(tkz.consume("a"));
+        tkz = new IterateTokenizer("a");
+        assertFalse(tkz.consume("abc"));
+        assertTrue(tkz.consume("a"));
+        tkz = new IterateTokenizer("abc");
+        assertFalse(tkz.consume("a"));
+        assertTrue(tkz.consume("abc"));
+    }
+}
