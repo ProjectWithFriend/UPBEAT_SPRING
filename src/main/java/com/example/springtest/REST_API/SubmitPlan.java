815730278e58866f267de8533f3c8675e4af578c
AuJung <natthaphong_th@cmu.ac.th>
2023-03-08 11:52:13 +0700

    - disable GamePage.java - add route submit plan to broadcast to all subscriber

--- src/main/java/com/example/springtest/REST_API/SubmitPlan.java
+++ src/main/java/com/example/springtest/REST_API/SubmitPlan.java
@@ -5,7 +5,10 @@ import com.example.springtest.BusinessLogic.Plan;
 import com.example.springtest.Exception.BadSubmitPlanException;
 import com.example.springtest.RequestBody.SubmitPlanReq;
 import com.example.springtest.websocket_route.GamePage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Controller
 @RequestMapping("/api")
 public class SubmitPlan {
+
+    @Autowired
+    private SimpMessagingTemplate template;
 
     @PostMapping("/submit_plan")
     public ResponseEntity<Object> submitPlan(@RequestBody SubmitPlanReq submitPlanReq) {
@@ -26,7 +33,7 @@ public class SubmitPlan {
                 throw new BadSubmitPlanException("Player id is empty");
             }
             Game updategame = Plan.submitPlan(submitPlanReq.getConstruction_plan(), submitPlanReq.getPlayer_id());
-            GamePage.broadcastGameUpdate(updategame); //TODO : discuss with team about this
+            template.convertAndSend("/topic/gameUpdate", updategame);
             return ResponseEntity.ok(updategame);
         } catch (RuntimeException e) { //TODO: catch specific exceptions from parsing
             throw new BadSubmitPlanException(e.getMessage());
@@ -36,6 +43,7 @@ public class SubmitPlan {
 
 
     //TODO : mock test for this class
+    GamePage gamePage = new GamePage();
     @PostMapping("/submit_plan_mock")
     public ResponseEntity<Object> submitPlanMock(@RequestBody SubmitPlanReq submitPlanReq) {
         try {
@@ -46,7 +54,6 @@ public class SubmitPlan {
                 throw new BadSubmitPlanException("Player id is empty");
             }
             Game updategame = Plan.submitPlanMock(submitPlanReq.getConstruction_plan(), submitPlanReq.getPlayer_id());
-            GamePage.broadcastGameUpdate(updategame); //TODO : discuss with team about this
             return ResponseEntity.ok(updategame);
         } catch (RuntimeException e) { //TODO: catch specific exceptions from parsing
             throw new BadSubmitPlanException(e.getMessage());
@@ -54,3 +61,5 @@ public class SubmitPlan {
 //        return null;
     }
 }
+
+
--- src/main/java/com/example/springtest/websocket_route/GamePage.java
+++ src/main/java/com/example/springtest/websocket_route/GamePage.java
@@ -1,16 +1,9 @@
 package com.example.springtest.websocket_route;
 
-import Game.Game;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@MessageMapping("/game")
+@Component
 public class GamePage {
-
-    @SendTo("/topic/gameUpdate")
-    public static Game broadcastGameUpdate(Game game) {
-        return game;
-    }
 }
