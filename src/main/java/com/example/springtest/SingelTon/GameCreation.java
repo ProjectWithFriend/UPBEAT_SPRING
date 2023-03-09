469de9d2c8721f4f637b277deb747c7daf98257e
AuJung <natthaphong_th@cmu.ac.th>
2023-03-09 09:13:31 +0700

    - fix bug that occur when createGame with the same hashCode but got different game

--- src/main/java/com/example/springtest/SingelTon/GameCreation.java
+++ src/main/java/com/example/springtest/SingelTon/GameCreation.java
@@ -8,20 +8,17 @@ import java.util.Map;
 
 public class GameCreation {
     private static Game gameCreation;
-    private static final Map<String[],Game> map = new HashMap<>();
+    private static final Map<String,Game> map = new HashMap<>();
 
     public static Game getGame(String nameP1,String nameP2){
-        if(map.isEmpty() || !map.containsKey(new String[]{nameP1,nameP2})){
+        String gameHash = nameP1 + nameP2;
+        if(map.isEmpty() || !map.containsKey(gameHash)){
             gameCreation = GameUtils.createGame(nameP1, nameP2);
-            map.put(new String[]{nameP1,nameP2},gameCreation);
+            map.put(gameHash,gameCreation);
             return gameCreation;
         }else{
-            if(map.get(new String[]{nameP1,nameP2}) == null){
-                gameCreation = GameUtils.createGame(nameP1, nameP2);
-                map.put(new String[]{nameP1,nameP2},gameCreation);
-                return gameCreation;
-            }
+            return map.get(gameHash);
         }
-        return null;
+//        return null;
     }
 }
